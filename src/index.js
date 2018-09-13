fb36078648a681732a0e483cae3de0c10bfcbb66
Andrei Prudnikau <prudnikov888@gmail.com>
2018-09-13 22:48:09 +0300

    Zeroes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+    let result = 0;
+    for (let i = 5; i <= number; i = i + 5) {
+        let num = i;
+        while (num % 5 === 0) {
+            num = num / 5;
+            result++;
+        }
+    }
+    return result;
+};
